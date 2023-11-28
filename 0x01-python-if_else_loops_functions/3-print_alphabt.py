481e57a83ae8dc9078108582bbbe24ab35bccc71
Law93D <lawtaden@gmail.com>
2023-11-28 10:51:40 -0800

    alphabet without q & e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for char in range(97, 123):
-    if chr(char) not in ['q', 'e']:
-        print(chr(char), end='')
+    if char in [101, 113]:
+        continue
+    print("{}".format(chr(char)), end='')
