67e1902813f8b016e6c286472a472c3162ce8cac
Law93D <lawtaden@gmail.com>
2023-12-03 11:09:02 -0800

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-    a = 1
-    b = 2
-    result = add(a, b)
+a = 1
+b = 2
+result = add(a, b)
 print("{a} + {b} = {result}".format(a=a, b=b, result=result))
