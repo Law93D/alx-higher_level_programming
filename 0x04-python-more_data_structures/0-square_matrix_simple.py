5a2f34886523a8b801c7dc28ba82c59cac257a3e
Law93D <lawtaden@gmail.com>
2023-12-06 12:38:23 -0800

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,3 @@
 #!/usr//bin/python3
 def square_matrix_simple(matrix=[]):
-    sq_matrix = matrix.copy()
-
-    for num in range(len(matrix)):
-        sq_matrix[num] = list(map(lambda x: x**2, matrix[num]))
-
-    return sq_matrix
+    return [[x ** 2 for x in row] for row in matrix]
