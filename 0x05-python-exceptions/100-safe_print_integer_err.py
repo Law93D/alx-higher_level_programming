5058ea5dcbfab57b9ffdb3194e87f93a0b2b74bf
Law93D <lawtaden@gmail.com>
2023-12-18 12:30:46 -0800

    integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,5 +6,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(int(value)))
         return True
     except (ValueError, TypeError):
-        print("Exception: []".format(sys.exc_info()[1]), file=sys.stderr)
+        print("Exception: '{}' is not a valid integer.".format(value), file=sys.stderr)
         return False
