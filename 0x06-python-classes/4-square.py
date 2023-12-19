078522c9e8c559c288d8ca830ff976092e5362ed
Law93D <lawtaden@gmail.com>
2023-12-19 12:27:57 -0800

    access

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a Square class with a private instance attribute."""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0):
+        """Initializes the Square instance."""
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        """Getter for size attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size attribute"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates the area of the square."""
+        return self.__size ** 2
