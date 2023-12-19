290d247030f8fd553516070433abf668d3c966b2
Law93D <lawtaden@gmail.com>
2023-12-19 12:22:21 -0800

    area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a Square class with a private instance attribute."""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0):
+        """Initializes the Square instance."""
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Calculates the area of the square."""
+        return self.__size ** 2
