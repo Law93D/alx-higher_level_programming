2c8205cbf62156014f61c7ac64e6759dd1e587d0
Law93D <lawtaden@gmail.com>
2023-12-19 12:07:45 -0800

    with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Square class with a private instance attribute."""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size):
+        """Initializes the Square instance."""
+        self.__size = size
