cb54dc839c8166b9b690963ae72653c78e4959f3
Law93D <lawtaden@gmail.com>
2023-12-19 12:33:28 -0800

    printing

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Defines a Square class with a private instance attribute."""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0):
+        """Initializes the Square instance."""
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        """Getter for size attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size attribute"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square with '#' character."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
