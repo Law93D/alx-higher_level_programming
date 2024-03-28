dbf3d9da4a17f71ae216129036d7f4f1292ef44d
Law93D <lawtaden@gmail.com>
2024-03-28 13:54:42 -0700

    python network_1

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Prints 10 commits (from the most recent to oldest) of the repository "rails"
+by the user "rails"
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    response = requests.get(url)
+    commits = response.json()
+    for commit in commits[:10]:
+        sha = commit.get("sha")
+        author_name = commit.get("commit").get("author").get("name")
+        print(f"{sha}: {author_name}")
