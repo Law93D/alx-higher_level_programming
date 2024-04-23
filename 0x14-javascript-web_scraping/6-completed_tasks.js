25308ba9bb96517e84f9d984761fff05c21957d3
Law93D <lawtaden@gmail.com>
2024-04-23 20:59:03 +0200

    web scrapping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,29 +5,29 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 request(apiUrl, function (error, response, body) {
- if (!error && response.statusCode === 200) {
-   try {
-     const todos = JSON.parse(body);
+  if (!error && response.statusCode === 200) {
+    try {
+      const todos = JSON.parse(body);
 
-     const completed = {};
+      const completed = {};
 
-     todos.forEach((todo) => {
-      if (todo.completed) {
-        if (completed[todo.userId] === undefined) {
-          completed[todo.userId] = 1;
-        } else {
-          completed[todo.userId]++;
+      todos.forEach((todo) => {
+        if (todo.completed) {
+          if (completed[todo.userId] === undefined) {
+            completed[todo.userId] = 1;
+          } else {
+            completed[todo.userId]++;
+          }
         }
-       }
-     });
+      });
 
-   const output = `{${Object.entries(completed).map(([key, value]) => ` '${key}': ${value}`).join(',\n ')} }`;
+      const output = `{${Object.entries(completed).map(([key, value]) => ` '${key}': ${value}`).join(',\n ')} }`;
 
-   console.log(Object.keys(completed).length > 2 ? output : completed);
-  } catch (parseError) {
-    console.error('Error parsing JSON:', parseError);
+      console.log(Object.keys(completed).length > 2 ? output : completed);
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
+    }
+  } else {
+    console.error('Error:', error);
   }
- } else {
-   console.error('Error:', error);
- }
 });
